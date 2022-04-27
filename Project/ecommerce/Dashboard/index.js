c5682a4ce4bc3a7f6ebae5de8575f585d33dcb9d
ArchanaCS <103047900+ArchanaCS@users.noreply.github.com>
2022-04-27 17:41:54 +0530

    Dashboard of ecommerce and Projectmanagement
    Dashboard of ecommerce and Projectmanagement(SQL queries for create and insert )User validate API

--- Project/ecommerce/Dashboard/index.js
+++ Project/ecommerce/Dashboard/index.js
@@ -6,11 +6,11 @@ var con = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
-        password: "password",
+        password: "password1234",
         database: "ecommercetasks"
     });
 // Dashboard topbar
-app.post('/dashtop', function (req, res) {
+app.post('/topbarpart1', function (req, res) {
     con.connect(function (err) {
         if (err) {
             console.log(err);
@@ -18,9 +18,79 @@ app.post('/dashtop', function (req, res) {
         else {
             console.log("Connected");
         }
-        var sql = " ";
+        var sql = "select count(id) as productcount from tblproduct ;";
 
-        con.query(sql, function (err, result) {
+
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log("Data inserted!!!!");
+                res.send(result);
+            }
+        });
+    });
+})
+app.post('/topbarpart2', function (req, res) {
+    con.connect(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Connected");
+        }
+        var sql = "select count(id) as ordercount from tblorderhdr;";
+
+        con.query(sql, function (err, result) 
+        {
+
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log("Data inserted!!!!");
+                res.send(result);
+            }
+        });
+    });
+})
+
+app.post('/orderamtgraph', function (req, res) {
+    con.connect(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Connected");
+        }
+        var sql = "SELECT date(dtCreatedOn), SUM(txtOrderAmount) AS OrderAmount FROM tblorderhdr  GROUP BY date(dtCreatedOn);";
+
+        con.query(sql, function (err, result) 
+        {
+
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log("Data inserted!!!!");
+                res.send(result);
+            }
+        });
+    });
+})
+
+app.post('/ordercountgraph', function (req, res) {
+    con.connect(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Connected");
+        }
+        var sql = "select date(dtCreatedOn),count(id) from tblorderhdr group by(dtCreatedOn);";
+
+        con.query(sql, function (err, result) 
+        {
 
             if (err) {
                 console.log(err);
--- Project/ecommerce/Signup/signup.js
+++ Project/ecommerce/Signup/signup.js
@@ -6,7 +6,7 @@ var con = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
-        password: "password",
+        password: "password1234",
         database: "ecommercetasks"
     });
 // country fetch

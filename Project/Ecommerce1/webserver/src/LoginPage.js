aacded9c7796803717efa15ecea674446f7bcae2
ArchanaCS <103047900+ArchanaCS@users.noreply.github.com>
2022-05-10 15:16:59 +0530

    Initial commit
    LoginHtml

--- Project/Ecommerce1/webserver/src/LoginPage.js
+++ Project/Ecommerce1/webserver/src/LoginPage.js
@@ -1,21 +1,31 @@
-import "./style.css"
-function LoginPage()
-{
-    return(
-    <div>
-       <label className="labelstyle"><h1>Login</h1> </label> 
-       <div>
-           <label className="labelstyle" >UserName</label>
-           <input type="text"/>
-       </div>
-       <div>
-           <label className="labelstyle">Password</label>
-           <input type="text"/>
-       </div>
-       <div>
-           <button>Login</button>
-       </div>
+import { useNavigate } from "react-router-dom";
+import "./style.css";
+function LoginPage() {
+    const navigate=useNavigate();
+    function newuser()
+    {
+        
+        navigate("/signup")
+
+    }
+  return (
+    <div class="container">
+      <label className="labelstyle">
+        <h1>Login</h1>{" "}
+      </label>
+      <div className="labelstyle">
+        <label >UserName</label>
+        <input type="text" className="textstyle" />
+      </div>
+      <div className="labelstyle">
+        <label >Password</label>
+        <input type="text" className="textstyle" />
+      </div>
+      <div className="labelstyle">
+        <button className="btn">Login</button>
+        <p className="userstyle" onClick={newuser}>New User?</p>
+      </div>
     </div>
-    )
+  );
 }
 export default LoginPage;
--- Project/Ecommerce1/webserver/src/Navigation.js
+++ Project/Ecommerce1/webserver/src/Navigation.js
@@ -4,16 +4,18 @@ import LoginPage from "./LoginPage";
 import ProductList from "./ProductList";
 import SignUp from "./SignUp";
 
-function Navigation(){
-  return(<div>
- <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />}></Route>
-      <Route path="/signup" element={<SignUp />}></Route> 
-      <Route path="/dashboard" element={<Dashboard />}></Route>
-      <Route path="/productlist" element={<ProductList/>}></Route>
-    </Routes>
-  </BrowserRouter>
-  </div>)
-};
+function Navigation() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/productlist" element={<ProductList />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 export default Navigation;

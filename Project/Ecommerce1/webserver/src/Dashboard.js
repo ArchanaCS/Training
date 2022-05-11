0dc7bd0cdeeab7803e799725c5a81fc3d9ea32c1
ArchanaCS <103047900+ArchanaCS@users.noreply.github.com>
2022-05-11 09:33:27 +0530

    initial commit
    dashboard

--- Project/Ecommerce1/webserver/src/Dashboard.js
+++ Project/Ecommerce1/webserver/src/Dashboard.js
@@ -1,21 +1,34 @@
 import "./style.css";
 function Dashboard() {
   return (
-    <div>
+    <div >
       {/* USer name in dashboard */}
       <div className="dashtop">
         <label>User</label>
       </div>
 
       {/* side menu */}
-      <div className="sidebar">
-          <nav>
-              <li>Menu</li>
+         <div className="sidebar">
+          <div className="menu">
+          <nav >
+              <li >Home</li>
               <li>Products</li>
               <li>Orders</li>
               <li>Logout</li>
           </nav>
-      </div>
+          </div>
+          </div>
+
+          {/* body part to list  */}
+          <div>
+              <table>
+                  <th>Id</th>
+                  <th> Product</th>
+                  <th>Price</th>
+                  <th>Tax</th>
+              </table>
+          </div>
+
     </div>
   );
 }

a5852e6a60eaea2c4efa889a7d4f3228e159390d
ArchanaCS <103047900+ArchanaCS@users.noreply.github.com>
2022-05-16 10:34:08 +0530

    SnowBird

--- Training/newsample/frontend/src/Sample.js
+++ Training/newsample/frontend/src/Sample.js
@@ -5,33 +5,38 @@ import order from "./order.png";
 import logout from "./logout.png";
 
 function Sample() {
-
-  
   return (
     <div>
       <div className="outer">
         <div className="firstrow">
           <label>Archana</label>
-          </div>
-          <div className="secondrow">
-            <div className="firstcolmn">
+        </div>
+        <div className="secondrow">
+          <div className="firstcolmn">
             <nav>
-             <li> <img src={home}></img>    Home</li>
-            <li>  <img src={cart}></img>    Products</li>
-            <li>  <img src={order}></img>   Order</li>
-            <li>  <img src={logout}></img>  Logout</li>
+              <li>
+                {" "}
+                <img src={home}></img> Home
+              </li>
+              <li>
+                {" "}
+                <img src={cart}></img> Products
+              </li>
+              <li>
+                {" "}
+                <img src={order}></img> Order
+              </li>
+              <li>
+                {" "}
+                <img src={logout}></img> Logout
+              </li>
             </nav>
-            </div>
-          
-          <div className="secondcolmn"> 
-           <div className="scfirstrow">
-             Dashboard
-           </div>
-           <div className="scsecondrow">
-             
-           </div>
-           </div>
-          
+          </div>
+
+          <div className="secondcolmn">
+            <div className="scfirstrow">Dashboard</div>
+            <div className="scsecondrow"></div>
+          </div>
         </div>
       </div>
     </div>

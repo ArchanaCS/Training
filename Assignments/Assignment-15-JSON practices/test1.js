762c06f624530229d90fba23667bc9af1849f0c2
ArchanaCS <103047900+ArchanaCS@users.noreply.github.com>
2022-05-17 09:22:28 +0530

    assignment json16May

---
+++ Assignments/Assignment-15-JSON practices/test1.js
@@ -0,0 +1,233 @@
+var school = {
+  school1: {
+    one: [
+      {
+        rollno: "1",
+        name: "aa",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+      {
+        rollno: "2",
+        name: "bb",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+      {
+        rollno: "3",
+        name: "cc",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+    ],
+    two: [
+      {
+        rollno: "1",
+        name: "aa",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+      {
+        rollno: "2",
+        name: "bb",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+      {
+        rollno: "3",
+        name: "cc",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+      {
+        rollno: "4",
+        name: "dd",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+      {
+        rollno: "5",
+        name: "ee",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+    ],
+    three: [
+      {
+        rollno: "1",
+        name: "aa",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+      {
+        rollno: "2",
+        name: "bb",
+        mark: {
+          English: "50",
+          Maths: "100",
+          Hindi: "200",
+        },
+      },
+    ]},
+    school2: {
+      one: [
+        {
+          rollno: "1",
+          name: "aa",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "2",
+          name: "bb",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "3",
+          name: "cc",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+      ],
+      two: [
+        {
+          rollno: "1",
+          name: "aa",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "2",
+          name: "bb",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "3",
+          name: "cc",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "4",
+          name: "dd",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "5",
+          name: "ee",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+      ],
+      three: [
+        {
+          rollno: "1",
+          name: "aa",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "2",
+          name: "bb",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "3",
+          name: "cc",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "4",
+          name: "dd",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+        {
+          rollno: "5",
+          name: "ee",
+          mark: {
+            English: "50",
+            Maths: "100",
+            Hindi: "200",
+          },
+        },
+      ],
+    },
+  }
+
+
+//2nd school-- 3rdclass--4thstudent--english
+console.log(school.school2.three[3].mark.English);
+
+//1st school--2ndclass--5thstudent--maths
+
+console.log(school.school1.two[4].mark.Maths);
+
